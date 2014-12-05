ae8f6ba82654919be9f6375bab40990f870ac0cf
Syzngit <sunyao_will@163.com>
2014-12-05 22:08:47 +0800

    医院信息页面信息的返回

--- app/controllers/HospitalController.php
+++ app/controllers/HospitalController.php
@@ -18,8 +18,44 @@ class HospitalController extends BaseController
     /**
      * 获取医院页面的所有信息并返回
      */
-    function getHospitalInfo()
+    function getHospitalInfo($hospitalId)
     {
+        $hospital = new Hospital();
+        // 医院名称
+        $hospitalName = $hospital->getHospitalName($hospitalId);
 
+        // 医院等级
+        $hospitalLevel = $hospital->getHospitalLevel($hospitalId);
+
+        // 医院地址
+        $hospitalAddress = $hospital->getHospitalAddress($hospitalId);
+
+        // 医院电话
+        $hospitalTel = $hospital->getHospitalTel($hospitalId);
+
+        // 医院网址
+        $hospitalUrl = $hospital->getHospitalUrl($hospitalId);
+
+        // 医院简介
+        $hospitalIntroduction = $hospital->getHospitalIntroduction($hospitalId);
+
+        // 医院预约周期
+        $hospitalReservationCycle = $hospital->getHospitalReservationCycle($hospitalId);
+
+        // 医院放号时间
+        $hospitalRegistrationOpenTime =  $hospital->getHospitalRegistrationOpenTime($hospitalId);
+
+        // 医院停挂时间
+        $hospitalRegistrationClosedTime = $hospital->getHospitalRegistrationClosedTime($hospitalId);
+
+        // 医院退号时间
+        $hospitalRegistrationCancelDeadline = $hospital->getHospitalRegistrationCancelDeadline($hospitalId);
+
+        $hospitalInformation = array('name'=>$hospitalName,'level'=>$hospitalLevel,'address'=>$hospitalAddress,
+            'tel'=>$hospitalTel,'url'=>$hospitalUrl,'introduction'=>$hospitalIntroduction,'reservation_cycle'=>$hospitalReservationCycle,
+            'registration_open_time'=>$hospitalRegistrationOpenTime,'registration_closed_time'=>$hospitalRegistrationClosedTime,
+            'registration_cancel_deadline'=>$hospitalRegistrationCancelDeadline);
+
+        return json_encode($hospitalInformation);
     }
 }
--- app/models/Hospital.php
+++ app/models/Hospital.php
@@ -20,4 +20,119 @@ class Hospital
     // 设置hospital表的主键
     protected $primaryKey = 'hospital_id';
 
+
+    /**
+     * 获取医院名称
+     * @param $id
+     * @return mixed
+     */
+    public function getHospitalName($id)
+    {
+        $name = DB::table('hospital')->where('hospital_id', $id)->pluck('hospital_name');
+        return $name;
+    }
+
+
+    /**
+     * 获取医院等级
+     * @param $id
+     * @return mixed
+     */
+    public  function  getHospitalLevel($id)
+    {
+        $level = DB::table('hospital')->where('hospital_id', $id)->pluck('level');
+        return $level;
+    }
+
+    /**
+     * 获取医院地址
+     * @param $id
+     * @return mixed
+     */
+    public function getHospitalAddress($id)
+    {
+        $district_id = DB::table('hospital')->where('hospital_id', $id)->pluck('district_id');
+        $district_name = DB::table('hospital')->where('hospital_id', $id)->pluck('district_name');
+        /*
+        $hospitalAddress = .$district_name;
+        */
+        return $hospitalAddress;
+    }
+
+    /**
+     *  获取医院电话
+     * @param $id
+     * @return mixed
+     */
+    public function getHospitalTel ($id)
+    {
+        $tel= DB::table('hospital')->where('hospital_id', $id)->pluck('hospital_tel');
+        return $tel;
+    }
+
+    /**
+     * 获取医院网址
+     * @param $id
+     * @return mixed
+     */
+    public function getHospitalUrl($id)
+    {
+        $url = DB::table('hospital')->where('hospital_id', $id)->pluck('hospital_url');
+        return $url;
+    }
+
+    /**
+     * 获取医院简介
+     * @param $id
+     * @return mixed
+     */
+    public function getHospitalIntroduction ($id)
+    {
+        $introduction = DB::table('hospital')->where('hospital_id', $id)->pluck('introduction');
+        return $introduction;
+    }
+
+    /**
+     * 获取医院预约周期
+     * @param $id
+     * @return mixed
+     */
+    public function  getHospitalReservationCycle ($id)
+    {
+        $reservationCycle = DB::table('hospital')->where('hospital_id', $id)->pluck('reservation_cycle');
+        return $reservationCycle;
+    }
+
+    /**
+     * 获取医院放号时间
+     * @param $id
+     * @return mixed
+     */
+    public function  getHospitalRegistrationOpenTime($id)
+    {
+        $hospitalRegistrationOpenTime= DB::table('hospital')->where('hospital_id', $id)->pluck('registration_open_time');
+        return $hospitalRegistrationOpenTime;
+    }
+
+    /**
+     * 获取医院停挂时间
+     * @param $id
+     * @return mixed
+     */
+    public function  getHospitalRegistrationClosedTime($id)
+    {
+        $hospitalRegistrationClosedTime=DB::table('hospital')->where('hospital_id', $id)->pluck('registration_closed_time');
+        return $hospitalRegistrationClosedTime;
+    }
+
+    /**
+     * 获取医院退号时间
+     * @param $id
+     * @return mixed
+     */
+    public function  getHospitalRegistrationCancelDeadline($id)
+    {
+        $hospitalRegistrationCancelDeadline = DB::table('hospital')->where('hospital_id', $id)->pluck('registration_cancel_deadline');
+        return $hospitalRegistrationCancelDeadline;
+    }
 }
